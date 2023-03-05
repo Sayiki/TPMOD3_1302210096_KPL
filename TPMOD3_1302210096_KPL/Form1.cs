c00fbf48a7a6075b1f37bbe2d365316067dc0fb4
Sayiki <arzaqajradika1@gmail.com>
2023-03-05 14:35:26 +0700

    Add project files.

---
+++ TPMOD3_1302210096_KPL/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TPMOD3_1302210096_KPL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Halo " + textBox2.Text;
+        }
+    }
+}
